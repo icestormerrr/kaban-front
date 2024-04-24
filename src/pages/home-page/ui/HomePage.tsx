f82a089fbf9b64307147a7ec7b2fa8f68e489cc0
Iurkin Vadim <VIurkin@croc.ru>
2024-04-25 00:21:12 +0300

    refactor: home page translation

--- src/pages/home-page/ui/HomePage.tsx
+++ src/pages/home-page/ui/HomePage.tsx
@@ -1,13 +1,17 @@
 import React, { FC } from "react";
+import { useTranslation } from "react-i18next";
 
 import { GlassContainer } from "src/shared/ui";
+import { useProjectId } from "src/entities/project";
 
 import classes from "./HomePage.module.scss";
 
 const HomePage: FC = () => {
+  const { t } = useTranslation();
+
   return (
     <GlassContainer className={classes.container}>
-      <h3 className={classes.title}>Welcome to KaBan, have a nice work :3</h3>
+      <h3 className={classes.title}>{t("Welcome to KaBan, have a nice work :3")}</h3>
     </GlassContainer>
   );
 };
